7206ef4300d78a2aacbf0b91beb4429187a6fd8a
Jhon-crypt <johnoladele690@gmail.com>
2022-08-05 00:34:05 -0700

    chore : update the front end library to a cdn

--- index.php
+++ index.php
@@ -65,11 +65,10 @@ class landingPage{
 
                     <title>pasteX</title>
 
-                    <!-- boostrap css -->
-                    <link href="public/assets/bootstrap-library/css/bootstrap.min.css" rel="stylesheet"/>
+                    <!-- CSS only -->
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-                    <!-- font awesome icon css -->
-                    <link href="public/assets/font-awesome-library/css/font-awesome.min.css" rel="stylesheet"/>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
                     <!- custom css -->
                     <link href="./public/assets/css/landing-page/index.css" rel="stylesheet"/>
